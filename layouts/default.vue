67ff12f80660d685bc9b897957afc7d45ec83d05
Andy Grabow <andy@freilandkiwis.de>
2021-05-01 18:32:29 +0200

    wir haben eine leere seite

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  div.page
+    nuxt
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Home', icon: 'home', to: { name: 'index' } },
+        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+body
+  background: #aaa
+  height: 100vh
+  display: flex
+  align-items: center
+  justify-content: center
+
+.page
+  background: white
+  width: 100vw
+  max-width: 400px
+  height: 100vh
+
+</style>
